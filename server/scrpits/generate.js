523f894ebae3cb5d8b91b6153e580ce49a74a526
jarryingnut <rabhishekranjan@gmail.com>
2022-12-05 19:39:57 +0530

    intialised files

---
+++ server/scrpits/generate.js
@@ -0,0 +1,3 @@
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
+const secp = require("ethereum-cryptography/secp256k1");
